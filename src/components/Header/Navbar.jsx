af10119a710d476eb527efeba79e1a1225e67923
faisalbari193 <faisalbari193@gmail.com>
2025-04-18 13:32:05 +0600

    added some data

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
               {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Book Store</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
@@ -28,7 +28,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn">Sign-Up</a>
         </div>
       </div>
     );
